abf7b0444018682c4310e52ce96a5d87598c83bc
woody <sarawut.core@gmail.com>
2024-08-29 16:31:15 +0700

    Complete project

---
+++ lib/weather/view/weather_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather/settings/view/settings_page.dart';
+import 'package:flutter_weather/weather/cubit/weather_cubit.dart';
+import 'package:flutter_weather/weather/widgets/widgets.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () => Navigator.of(context).push<void>(
+              SettingsPage.route(),
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: BlocBuilder<WeatherCubit, WeatherState>(
+          builder: (context, state) {
+            return switch (state.status) {
+              WeatherStatus.initial => const WeatherEmpty(),
+              WeatherStatus.loading => const WeatherLoading(),
+              WeatherStatus.failure => const WeatherError(),
+              WeatherStatus.success => WeatherPupulated(
+                  weather: state.weather,
+                  units: state.temperatureUnits,
+                  onRefresh: () {
+                    return context.read<WeatherCubit>().refreshWeather();
+                  }),
+            };
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.search, semanticLabel: 'Search'),
+        onPressed: () async {
+          final city = await Navigator.of(context).push(SearchPage.route());
+          if (!context.mounted) return;
+          await context.read<WeatherCubit>().fetchWeather(city);
+        },
+      ),
+    );
+  }
+}
